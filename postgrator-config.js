de13218735cab6a766c157fb850e879ab28530f7
rshuken <ryanshuken@gmail.com>
2020-11-04 09:41:47 -0700

    trying to fix deploy issues

--- postgrator-config.js
+++ postgrator-config.js
@@ -1,10 +1,10 @@
 /* eslint-disable strict */
 require('dotenv').config();
-
+console.log(process.env.DATABASE_URL)
 module.exports = {
   migrationsDirectory: 'migrations',
   driver: 'pg',
   'connectionString': (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
-    : process.env.DATABASE_URL,
+    : process.env.DATABASE_URL
 };
