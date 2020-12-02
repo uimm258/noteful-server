84c5714f20a4c6cfecad8d0aa254bd420f87d641
uimm258 <uimm258@gmail.com>
2020-12-02 15:08:38 -0800

    nth deploy

--- seeds/seed.noteful_folders.sql
+++ seeds/seed.noteful_folders.sql
@@ -1,9 +1,9 @@
-insert into folders (id, name)
+insert into folders (name)
 values
-('1','Important'),
-('2','Super'),
-('3','Spangly'),
-('4','Personal'),
-('5','Work'),
-('6','Family'),
-('7','Vacation');
+('Important'),
+('Super'),
+('Spangly'),
+('Personal'),
+('Work'),
+('Family'),
+('Vacation');
